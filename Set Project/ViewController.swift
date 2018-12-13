672787481e6465b351487671683c091ad1fcd819
Justin Biggs <jhbscholastic@gmail.com>
2018-12-13 11:41:03 -0600

    Initial Commit

---
+++ Set Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set Project
+//
+//  Created by Justin Biggs on 12/13/18.
+//  Copyright © 2018 MindFrame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
